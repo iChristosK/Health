2b424aa887784d178286e615633866e074da14da
iChristosK <16455384+iChristosK@users.noreply.github.com>
2023-06-15 11:52:22 +0200

    Health App for macOS

---
+++ Health/HealthApp.swift
@@ -0,0 +1,18 @@
+//
+//  HealthApp.swift
+//  Health
+//
+//  Created by Jordan Singer on 7/14/20.
+//
+
+import SwiftUI
+
+@available(macOS 11.0, *)
+@main
+struct HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Health
-Mac Health App with SwiftUI
+Health app macOS Big Sur built in SwiftUI
+
+![Health](https://user-images.githubusercontent.com/110813/87485160-1f923380-c606-11ea-8a7e-9b12b2c0a852.png)
